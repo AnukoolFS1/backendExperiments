f0c95418df1486662e367aa3d80bc6753813346b
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-06 10:42:22 +0530

    some white-space correction and reinstall packages

--- models/db.js
+++ models/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
-
 async function connect() {
-    await mongoose.connect('mongodb+srv://anukool:fmdb1@learningmdb.cqck4.mongodb.net/revisionDB?retryWrites=true&w=majority&appName=LearningMDB')
-    console.log("DB Conneted!")
-
+    try{
+        await mongoose.connect('mongodb+srv://anukool:fmdb1@learningmdb.cqck4.mongodb.net/revisionDB?retryWrites=true&w=majority&appName=LearningMDB')
+        console.log("DB Conneted!")
+    }
+    catch(error){
+        console.log('Error occured')
+        console.log(error)
+    }
 }
 
 module.exports = connect
