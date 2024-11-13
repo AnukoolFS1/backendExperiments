ee0262afd4d43eb28ac99c980087af03b9a6907e
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-13 21:20:41 +0530

    connection way to existing database

--- db.js
+++ db.js
@@ -1,20 +1,44 @@
-const {MongoClient} = require('mongodb')
+const {MongoClient} = require('mongodb');
 
 const url = 'mongodb://localhost:27017';
 
-const client = new MongoClient(url);
+let dbConnection
 
-const dbName = "Project";
 
-async function main () {
-    await client.connect();
-    console.log('successfully connected with the database');
-    const db = client.db(dbName);
-    const collection = db.collection('documents');
-    collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
-
-    return 'done.'
+module.exports = {
+    connectDB : (cb) => {
+        MongoClient.connect(url)
+        .then((client) => {
+            dbConnection = client.db()
+            cb()
+        })
+        .catch(err => {
+            console.log(err)
+            return cb(err)
+        })
+    },
+    getDB: () => dbConnection
 }
 
 
-module.exports = main
+// const {MongoClient} = require('mongodb')
+
+// const url = 'mongodb://localhost:27017';
+
+// const client = new MongoClient(url);
+
+// const dbName = "Project";
+
+// async function main () {
+//     await client.connect();
+//     console.log('successfully connected with the database');
+//     const db = client.db(dbName);
+//     const collection = db.collection('documents');
+//     collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
+
+//     return 'done.'
+// }
+
+
+// module.exports = main
+
